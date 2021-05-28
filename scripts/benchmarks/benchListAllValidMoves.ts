4f34c84e0e8da2e5e7428057352fac07522a5edb
Ray Myers <raydog87@gmail.com>
2021-05-27 19:21:52 -0700

    Add move enumeration to public API ; start migrating to annotated move storage

--- scripts/benchmarks/benchListAllValidMoves.ts
+++ scripts/benchmarks/benchListAllValidMoves.ts
@@ -1,18 +1,11 @@
 import { ChessGame } from "../../src/datatypes/ChessGame.ts";
-import { Color } from "../../src/datatypes/Color.ts";
-import { listAllValidMoves } from "../../src/logic/listValidMoves.ts";
-// import { moveToAN } from "../../src/logic/moveFormats/algebraicNotation.ts";
 import { runBenchmarks, bench } from "../../testDeps.ts";
 
 
-// TODO: Get rid of the _getBoard hack...
-
-const giuocoPianoBoard = (ChessGame.NewStandardGame()
+const giuocoPiano = ChessGame.NewStandardGame()
   .move("e2-e4").move("e7-e5")
   .move("g1-f3").move("b8-c6")
-  // deno-lint-ignore no-explicit-any
-  .move("f1-c4").move("f8-c5") as any)
-  ._getBoard();
+  .move("f1-c4").move("f8-c5");
 
 
 bench({
@@ -20,7 +13,10 @@ bench({
   runs: 500,
   func(b) {
     b.start();
-    const moves = listAllValidMoves(giuocoPianoBoard, Color.White, true);
+    // This operation is pretty quick, so do it a bunch of times to get more signal:
+    for (let i=0; i<100; i++) {
+      const _moves = giuocoPiano.allMoves();
+    }
     b.stop();
   }
 })
--- src/datatypes/ChessGame.ts
+++ src/datatypes/ChessGame.ts
@@ -1,7 +1,7 @@
 // Note: the class in this file serves as the external API.
 
 import { buildStandardBoard } from "../logic/boardLayouts/standard.ts";
-import { listValidMoves } from "../logic/listValidMoves.ts";
+import { listValidMoves, listAllValidMoves } from "../logic/listValidMoves.ts";
 import { performMove } from "../logic/performMove.ts";
 import { Board } from "./Board.ts";
 import { ChessBadMove, ChessGameOver } from "./ChessError.ts";
@@ -10,16 +10,23 @@ import { coordFromAN, coordToAN } from "./Coord.ts";
 import { Move } from "./Move.ts";
 import { spaceGetColor, spaceHasData, spaceIsEmpty } from "./Space.ts";
 
+
 const MOVE_RE = /^([a-h][1-8])-?([a-h][1-8])$/i;
+
+type AnnotatedMove = {
+  move: Move,
+  san: string,
+}
 
 /**
  * A single chess game.
  */
 export class ChessGame {
+
   #state: GameState = GameState.active;
   #turn: Color = Color.White;
   #board: Board;
-  #moves: Move[] = [];
+  #moves: AnnotatedMove[] = [];
 
   private constructor(b: Board) {
     this.#board = b;
@@ -45,14 +52,14 @@ export class ChessGame {
   /**
    * Performs a chess move. The current player is assumed from the board state.
    *
-   * The format for the move is a short string, with the departing coordinate, and the destination cordinate right next
-   * to each other. (An optional hyphen can be included if you want.) For example, a classic King's opening would look
-   * like: "e2e4" (or "e2-e4", if you opt to include the hyphen.) This is not full algebraic notation, so don't add "x"
-   * to indicate capture, don't add "=Q" to indicate promotion, etc.
+   * The format for the move is a short string in UCI format, with both the departing and destination cordinate right
+   * next to each other. (An optional hyphen can be included if you want.) For example, a classic King's opening would
+   * look like: "e2e4" or "e2-e4", if you opt to include the hyphen. This is not full algebraic notation, so don't add
+   * "x" to indicate capture, don't add "=Q" to indicate promotion, etc.
    *
-   * If this move was a pawn reaching its final rank (and so it's promoting) you can add the additional `promote`
+   * If this move involves a pawn reaching its final rank (and so it's promoting) you can add the additional `promote`
    * parameter to indicate what piece type this should promote to. If omitted, Queen is assumed. If this parameter is
-   * included when this ISN'T a promotion, it'll just be ignored. Trying to promote to Pawn will error.
+   * included when this ISN'T a promotion, it'll just be ignored. Trying to promote to a Pawn will error.
    *
    * @param move A string like "b1c3", "d1-h5", etc.
    * @param promote
@@ -72,23 +79,51 @@ export class ChessGame {
     if (!spaceHasData(sp) || spaceIsEmpty(sp)) {
       throw new ChessBadMove(`${move}: Departing square (${coordToAN(from)}) is empty`);
     }
-    if (spaceGetColor(sp) !== this.#turn) {
-      throw new ChessBadMove(`${move}: It's ${colorToString(this.#turn)}'s turn`);
-    }
 
-    // Get the full list of moves for the piece at this spot:
-    const moves = listValidMoves(this.#board, from, true);
-    const picked = moves.find((move) => move.dest === dest);
+    // Get the full list of moves. We need the FULL list of moves (and not just the moves for this one piece) because
+    // computing the SAN for a move needs to change how the origin is represented based on which moves are currently
+    // available to the player:
+    const moves = listAllValidMoves(this.#board, this.#turn);
+    const picked = moves.find((move) => move.from === from && move.dest === dest);
     if (!picked) {
       throw new ChessBadMove(`${move}: Invalid move`);
     }
 
     // Else, we have the correct move! Apply to to our own board:
     performMove(this.#board, picked);
-    this.#moves.push(picked);
+    this.#moves.push({
+      move: picked,
+      san: "",
+      // san: al
+    });
     this.#turn = 1 - this.#turn;
 
     return this;
+  }
+
+  /**
+   * Returns all available moves for the current player. If a coord is provided, only moves for the piece at that coord
+   * will be returned. If that space is either empty, or holds a piece that belongs to the other player, an empty array
+   * will be returned.
+   * 
+   * Moves will be returned in UCI format, with the departing and destination coordinates right next to each other. So,
+   * "e2e4" will be returned for the classic King's opening.
+   * 
+   * @param coord An optional coordinate, formatted in algebraic notation, so like "a5".
+   */
+  allMoves(coord?: string): string[] {
+    let moves;
+    if (coord == null) {
+      moves = listAllValidMoves(this.#board, this.#turn, false);
+    } else {
+      const idx = coordFromAN(coord);
+      const sp = this.#board.get(idx);
+      if (spaceIsEmpty(sp) || spaceGetColor(sp) !== this.#turn) {
+        return [];
+      }
+      moves = listValidMoves(this.#board, idx, false);
+    }
+    return moves.map(move => `${coordToAN(move.from)}${coordToAN(move.dest)}`);
   }
 }
 
