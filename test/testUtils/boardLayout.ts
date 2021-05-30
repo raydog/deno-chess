981f7890c5d69963d6878fad27cd7ea97977ab33
Ray Myers <raydog87@gmail.com>
2021-05-30 15:10:04 -0700

    Fix the castle tests to work with the castle map

--- test/testUtils/boardLayout.ts
+++ test/testUtils/boardLayout.ts
@@ -1,6 +1,8 @@
 import { Board } from "../../src/datatypes/Board.ts";
-import { coordFromAN } from "../../src/datatypes/Coord.ts";
-import { Space } from "../../src/datatypes/Space.ts";
+import { buildCastleMap } from "../../src/datatypes/CastleMap.ts";
+import { Coord, coordFromAN } from "../../src/datatypes/Coord.ts";
+import { PieceType } from "../../src/datatypes/PieceType.ts";
+import { Space, spaceGetType, spaceHasMoved, spaceIsEmpty } from "../../src/datatypes/Space.ts";
 
 type BoardLayout = {
   [an: string]: Space;
@@ -22,5 +24,21 @@ export function boardLayout(layout: BoardLayout): Board {
     const coord = coordFromAN(key);
     out.set(coord, layout[key]);
   }
+  // Note: This assumes a stardard board layout:
+  out.setCastles(
+    buildCastleMap(
+      _coordIfValid(out, 0x04, 0x00),
+      _coordIfValid(out, 0x04, 0x07),
+      _coordIfValid(out, 0x74, 0x70),
+      _coordIfValid(out, 0x74, 0x77),
+    )
+  );
   return out;
+}
+
+function _coordIfValid(b: Board, king: Coord, rook: Coord): Coord {
+  const k = b.get(king), r = b.get(rook);
+  if (spaceIsEmpty(k) || spaceHasMoved(k) || spaceGetType(k) !== PieceType.King) { return 0x88; }
+  if (spaceIsEmpty(r) || spaceHasMoved(r) || spaceGetType(r) !== PieceType.Rook) { return 0x88; }
+  return rook;
 }
