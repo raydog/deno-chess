c1b4c55a5a480cc002ac891cdea503adb5fd835f
Ray Myers <raydog87@gmail.com>
2021-06-05 21:38:38 -0700

    Add method to select a move by SAN + WIP on tests

---
+++ src/logic/findMoveBySAN.ts
@@ -0,0 +1,112 @@
+import { ChessBadMove } from "../datatypes/ChessError.ts";
+import { coordFromAN } from "../datatypes/Coord.ts";
+import { Move } from "../datatypes/Move.ts";
+import { PieceType, PIECETYPE_KING } from "../datatypes/PieceType.ts";
+import { PIECETYPE_QUEEN } from "../datatypes/PieceType.ts";
+import { PIECETYPE_ROOK } from "../datatypes/PieceType.ts";
+import { PIECETYPE_KNIGHT } from "../datatypes/PieceType.ts";
+import { PIECETYPE_BISHOP } from "../datatypes/PieceType.ts";
+import { PIECETYPE_PAWN } from "../datatypes/PieceType.ts";
+import { spaceGetType } from "../datatypes/Space.ts";
+
+const PIECETYPE_MAP: { [type: string]: PieceType} = {
+  "": PIECETYPE_PAWN,
+  B: PIECETYPE_BISHOP,
+  N: PIECETYPE_KNIGHT,
+  R: PIECETYPE_ROOK,
+  Q: PIECETYPE_QUEEN,
+  K: PIECETYPE_KING,
+};
+
+const CASTLE_RE = /^(?:O-O(?:-O)?|0-0(?:-0)?)$/;
+const MOVE_RE = /^([BNRQK]?)([a-h]?)([1-8]?)(x?)([a-h][1-8])(?:=([BNRQ]))?(?:\s+|[!?]+|[+#]|[eE]\.?[pP]\.?|(?:1|0|1\/2|½)-(?:1|0|1\/2|½))*$/;
+// [1] - Piece capture
+// [2] - Departure file
+// [3] - Departure rank
+// [4] - Is capture?
+// [5] - Destination
+// [6] - Promotion?
+// [x] - Ignored extra stuff
+
+/**
+ * Locate a move by its standard algebraic notation representation.
+ * 
+ * Will throw an error if the SAN doesn't select exactly 1 move. (So be specific with the departure if there is that
+ * level of ambiguity...)
+ * 
+ * Details:
+ * - Ignores most annotations, like +, #, 1-0, e.p., ??, etc...
+ * - Castles can use either O's (uppercase leger) or 0's (digit zero), but must be consistent.
+ * 
+ * @param moves 
+ * @param san 
+ */
+export function findMoveBySAN(moves: Move[], san: string): Move {
+  san = san.trim();
+
+  // Castles:
+  if (CASTLE_RE.test(san)) {
+    return _selectMoveByInvariant(
+      moves,
+      san,
+      (san.length === 5)
+        ? move => (move.castleRookDest & 0x7) === 3 // O-O-O
+        : move => (move.castleRookDest & 0x7) === 5 // O-O
+    );
+  }
+
+  const match = san.match(MOVE_RE);
+  if (!match) {
+    throw new ChessBadMove(`Couldn't parse: ${san}`);
+  }
+
+  const move = _selectMoveByInvariant(
+    moves,
+    san,
+    move => (
+      // Check the destination + piece types first, since they're always present, and can fully identify the move
+      // in the majority of cases:
+      coordFromAN(match[5]) === move.dest &&
+      spaceGetType(move.what) === PIECETYPE_MAP[match[1]] &&
+
+      // Narrow things further by the departure parts + capture:
+      (match[2] ? (move.from & 0x7) === (match[2].charCodeAt(0) - 97) : true) &&
+      (match[3] ? ((move.from >>> 4) & 0x7) === (match[3].charCodeAt(0) - 49) : true) &&
+      (match[4] ? move.capture !== 0 : move.capture === 0)
+    )
+  );
+
+  // Finally, handle promotion, if requested.
+  if (match[6]) {
+    if (spaceGetType(move.what) !== PIECETYPE_PAWN) {
+      throw new ChessBadMove(`Piece at ${match[5]} is not a pawn`);
+    }
+    if (!move.promote) {
+      throw new ChessBadMove(`Pawn at ${match[5]} is not eligible for promotion`);
+    }
+    move.promote = PIECETYPE_MAP[match[6]];
+  }
+
+  return move;
+}
+
+
+function _selectMoveByInvariant(moves: Move[], san: string, fn: (m: Move) => boolean): Move {
+  let found: Move | null = null;
+
+  for (let i=0; i<moves.length; i++) {
+    const move = moves[i];
+    if (fn(move)) {
+      if (found) {
+        throw new ChessBadMove(`${san} is ambiguous`);
+      }
+      found = move;
+    }
+  }
+
+  if (found) {
+    return found;
+  }
+
+  throw new ChessBadMove(`${san} is not a valid move`);
+}
---
+++ test/findMoveBySAN.test.ts
@@ -0,0 +1,91 @@
+import { ChessBadMove } from "../src/datatypes/ChessError.ts";
+import { COLOR_BLACK, COLOR_WHITE } from "../src/datatypes/Color.ts";
+import { PIECETYPE_QUEEN } from "../src/datatypes/PieceType.ts";
+import { boardFromFEN } from "../src/logic/FEN/boardFromFEN.ts";
+import { findMoveBySAN } from "../src/logic/findMoveBySAN.ts";
+import { listAllValidMoves } from "../src/logic/listValidMoves.ts";
+import { moveToSAN } from "../src/logic/moveFormats/moveToSAN.ts";
+import { asserts } from "../testDeps.ts";
+
+Deno.test("Find Move by SAN > Pawn > Single", function () {
+  const board = boardFromFEN("5k2/8/8/8/2P5/8/1K6/8 w - - 0 1");
+  const moves = listAllValidMoves(board, COLOR_WHITE);
+  asserts.assert(findMoveBySAN(moves, "c5"));
+});
+
+Deno.test("Find Move by SAN > Pawn > Openings", function () {
+  const board = boardFromFEN("1k6/8/8/8/8/8/4PPP1/5K2 w - - 0 1");
+  const moves = listAllValidMoves(board, COLOR_WHITE);
+  asserts.assert(findMoveBySAN(moves, "f4"));
+  asserts.assert(findMoveBySAN(moves, "f3"));
+});
+
+Deno.test("Find Move by SAN > Pawn > Captures", function () {
+  const board = boardFromFEN("1k6/8/4b1p1/5P2/8/5n2/4P1P1/5K2 w - - 0 1");
+  const moves = listAllValidMoves(board, COLOR_WHITE);
+  asserts.assert(findMoveBySAN(moves, "exf3"));
+  asserts.assert(findMoveBySAN(moves, "gxf3"));
+  asserts.assert(findMoveBySAN(moves, "fxe6"));
+  asserts.assert(findMoveBySAN(moves, "fxg6"));
+});
+
+Deno.test("Find Move by SAN > Pawn > Promote", function () {
+  const board = boardFromFEN("2r3kr/3P1ppp/8/8/8/8/1K6/2n2q2 w - - 0 1");
+  const moves = listAllValidMoves(board, COLOR_WHITE);
+  const move1 = findMoveBySAN(moves, "dxc8=Q# 1-0");
+  asserts.assert(move1);
+  asserts.assertEquals(move1.promote, PIECETYPE_QUEEN);
+});
+
+Deno.test("Find Move by SAN > Pawn > En Passant", function () {
+  const board = boardFromFEN("5k2/8/8/3Pp3/8/8/1K6/8 w - e6 0 1");
+  const moves = listAllValidMoves(board, COLOR_WHITE);
+  asserts.assert(findMoveBySAN(moves, "dxe6"));
+});
+
+Deno.test("Find Move by SAN > Bishops > Normal Move", function () {
+  const board = boardFromFEN("8/5pk1/8/8/8/8/5B2/6K1 w - - 0 1");
+  const moves = listAllValidMoves(board, COLOR_WHITE);
+  asserts.assert(findMoveBySAN(moves, "Bc5"));
+});
+
+Deno.test("Find Move by SAN > Bishops > Ambiguous (Rank) Moves", function () {
+  const board = boardFromFEN("8/5pk1/1B6/8/8/8/1B6/6K1 w - - 0 1");
+  const moves = listAllValidMoves(board, COLOR_WHITE);
+  asserts.assertThrows(() => findMoveBySAN(moves, "Bd4"), ChessBadMove, "ambiguous");
+  asserts.assert(findMoveBySAN(moves, "B2d4"));
+});
+
+Deno.test("Find Move by SAN > Bishops > Ambiguous (File) Moves", function () {
+  const board = boardFromFEN("6k1/3p4/8/8/1B3B2/8/8/3K4 w - - 0 1");
+  const moves = listAllValidMoves(board, COLOR_WHITE);
+  asserts.assertThrows(() => findMoveBySAN(moves, "Bd6"), ChessBadMove, "ambiguous");
+  asserts.assert(findMoveBySAN(moves, "Bfd6"));
+});
+
+Deno.test("Find Move by SAN > Bishops > Ambiguous (Full) Moves", function () {
+  const board = boardFromFEN("1B4k1/3p4/8/8/1B3B2/8/8/3K4 w - - 0 1");
+  const moves = listAllValidMoves(board, COLOR_WHITE);
+  asserts.assertThrows(() => findMoveBySAN(moves, "Bd6"), ChessBadMove, "ambiguous");
+  asserts.assertThrows(() => findMoveBySAN(moves, "Bbd6"), ChessBadMove, "ambiguous");
+  asserts.assertThrows(() => findMoveBySAN(moves, "B4d6"), ChessBadMove, "ambiguous");
+  asserts.assert(findMoveBySAN(moves, "Bb4d6"));
+});
+
+Deno.test("Find Move by SAN > Bishops > Capture", function () {
+  const board = boardFromFEN("8/1b3pk1/8/8/8/5R2/8/6K1 w - - 0 1");
+  const moves = listAllValidMoves(board, COLOR_BLACK);
+  asserts.assert(findMoveBySAN(moves, "Bxf3"));
+});
+
+Deno.test("Find Move by SAN > Knight > Move", function () {
+  const board = boardFromFEN("5k2/8/1N3n2/8/8/8/1K6/8 w - - 0 1");
+  const moves = listAllValidMoves(board, COLOR_BLACK);
+  asserts.assert(findMoveBySAN(moves, "Nd5"));
+});
+
+Deno.test("Find Move by SAN > Knight > Capture", function () {
+  const board = boardFromFEN("5k2/8/5n2/3N4/8/8/1K6/8 w - - 0 1");
+  const moves = listAllValidMoves(board, COLOR_BLACK);
+  asserts.assert(findMoveBySAN(moves, "Nxd5"));
+});
