20a736573b425f7ca77a8265371bb69767a0a212
Ray Myers <raydog87@gmail.com>
2021-06-07 23:33:13 -0700

    Tweak the newVersion script

--- scripts/utilities/newVersion.sh
+++ scripts/utilities/newVersion.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
-PWD="$(dirname "$0")"
 VERSION="$1"
+TODAY="$(date '+%Y-%m-%d')"
+YEAR="$(date '+%Y')"
+DENO_LIB_URL="https://deno.land/x/chess@$VERSION/mod.ts"
+VERSION_RE="$(echo "$VERSION" | sed 's/[.]/\\./g')"
 
-if [[ ! "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-.*)?$ ]] ; then
+PWD="$(dirname "$0")"
+MAKEFILE_PATH="$PWD/../../Makefile"
+HISTORY_PATH="$PWD/../../HISTORY.md"
+README_PATH="$PWD/../../README.md"
+
+# Must be semver-ish:
+if [[ ! "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9]+)?$ ]] ; then
     >&2 echo "Error: Not a semver: $VERSION"
+    exit 1
+fi
+
+# History doc MUST have an entry for this specific version:
+if ! grep -Eq "^## $VERSION_RE( .*)?\$" "$HISTORY_PATH" ; then
+    >&2 echo "Error: HISTORY.md must have an entry for this version"
     exit 1
 fi
 
 echo "Releasing as: $VERSION"
 
-DENO_LIB_URL="https://deno.land/x/chess@$VERSION/mod.ts"
-
 # Edit the current README.md library version:
-sed -Ei 's|https://deno.land/x/chess@(.*?)/mod.ts|'$DENO_LIB_URL'|' "$PWD/../../README.md"
+sed -Ei 's|https://deno.land/x/chess@(.*?)/mod.ts|'$DENO_LIB_URL'|' "$README_PATH"
+
+# Edit the HISTORY.md file to include the current date for this version:
+sed -Ei "s|^## $VERSION_RE( .*)?|## $VERSION ($TODAY)|" "$HISTORY_PATH"
+
+# Update the two variables in the Makefile
+sed -Ei "s|^BundleVersion .*|BundleVersion = $VERSION|" "$MAKEFILE_PATH"
+sed -Ei "s|^BundleYear .*|BundleYear = $YEAR|" "$MAKEFILE_PATH"
 
 # Make a release commit:
 git commit --allow-empty -am "Release: $VERSION"
